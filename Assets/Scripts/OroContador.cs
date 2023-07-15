12b4dc45eb46938cdaed5bee261f35a0395c9bbb
Alberto <alberto12serrano@gmail.com>
2023-07-15 19:16:34 +0200

    Creados el resto de contadores
    Se han creado los contadores de Madera, Comida y Conocimiento

--- Assets/Scripts/OroContador.cs
+++ Assets/Scripts/OroContador.cs
@@ -3,21 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ContadorOro : MonoBehaviour
 {
 
     [SerializeField] private float oro;
-    [SerializeField] private Text contador;
+    [SerializeField] private TMP_Text contador;
     [SerializeField] private float oroPorSegundo;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        contador = GetComponent<Text>();
+        contador = GetComponent<TMP_Text>();
         oro = 0;
-        oroPorSegundo = 1f;
+        oroPorSegundo = 1.2f;
         
 
     }
